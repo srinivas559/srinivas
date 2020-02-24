0ec38b552b00915dfd837a6363316ed8dc1bad63
srinivas559 <40357055+srinivas559@users.noreply.github.com>
2020-02-24 22:41:59 +0530

    Using Throws Keyword in exception handling

---
+++ SampleException3.java
@@ -0,0 +1,23 @@
+package.Package4;
+
+import java.io.*;
+class SampleException3 { 
+  void myMethod(int num)throws IOException, ClassNotFoundException{ 
+     if(num==1)
+        throw new IOException("IOException Occurred");
+     else
+        throw new ClassNotFoundException("ClassNotFoundException");
+  } 
+} 
+
+public class Example1{ 
+  public static void main(String args[]){ 
+   try{ 
+     ThrowExample obj=new ThrowExample(); 
+     obj.myMethod(1); 
+   }catch(Exception ex){
+     System.out.println(ex);
+    } 
+  }
+}
+Output:
